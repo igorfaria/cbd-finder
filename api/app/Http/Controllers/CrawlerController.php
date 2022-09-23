4f578aaebcd5c9ee285818c85c0fbaac13ebcedf
Igor Faria <itfaria@gmail.com>
2022-09-23 07:45:59 +0100

    Initial controllers and models created

---
+++ api/app/Http/Controllers/CrawlerController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Crawler;
+use Illuminate\Http\Request;
+
+class CrawlerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Crawler  $crawler
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Crawler $crawler)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Crawler  $crawler
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Crawler $crawler)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Crawler  $crawler
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Crawler $crawler)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Crawler  $crawler
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Crawler $crawler)
+    {
+        //
+    }
+}
---
+++ api/app/Models/Address.php
@@ -0,0 +1,13 @@
+<?php
+
+// name,street,locality,city,zip,lat,long
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Address extends Model
+{
+    use HasFactory;
+}
